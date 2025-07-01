c9006720ccfeccec117de28ccac169df2ee6bd61
Tioma <tiomas15@gmail.com>
2025-07-01 14:05:13 +0300

    upad

--- client/src/context/categories/CategoriesContextWrapper.jsx
+++ client/src/context/categories/CategoriesContextWrapper.jsx
@@ -1,12 +1,22 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { initialCategoriesContext } from "./initialCategoriesContext";
 import { CategoriesContext } from "./CategoriesContext";
+import { UserContext } from "../user/UserContext";
 
 export function CategoriesContextWrapper(props) {
     const [categories, setCategories] = useState(initialCategoriesContext.categories);
+    const [featuredCategories, setFeaturedCategories] = useState(initialCategoriesContext.featuredCategories);
+
+    const { isLoggedIn } = useContext(UserContext);
 
     useEffect(() => {
-        fetch('http://localhost:5417/api/admin/categories', {
+        let apiUrl = 'http://localhost:5417/api/public/categories';
+
+        if (isLoggedIn) {
+            apiUrl = 'http://localhost:5417/api/admin/categories';
+        }
+
+        fetch(apiUrl, {
             method: 'GET',
             credentials: 'include',
         })
@@ -14,6 +24,20 @@ export function CategoriesContextWrapper(props) {
             .then(data => {
                 if (data.status === 'success') {
                     setList(data.list);
+                }
+            })
+            .catch(console.error);
+    }, [isLoggedIn]);
+
+    useEffect(() => {
+        fetch('http://localhost:5417/api/public/categories/featured', {
+            method: 'GET',
+            credentials: 'include',
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.status === 'success') {
+                    setFeaturedList(data.list);
                 }
             })
             .catch(console.error);
@@ -32,12 +56,18 @@ export function CategoriesContextWrapper(props) {
     function remove() {
     }
 
+    function setFeaturedList(data) {
+        setFeaturedCategories(() => data);
+    }
+
     const value = {
         categories,
+        featuredCategories,
         setList,
         create,
         edit,
         remove,
+        setFeaturedList,
     };
 
     return (
